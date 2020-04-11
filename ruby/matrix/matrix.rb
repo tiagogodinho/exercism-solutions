1ae01fc30e366895fe808ce33f856c7ab1d1a049
Tiago Godinho <tiagogodinho3@gmail.com>
2020-04-11 13:44:06 -0300

    Ruby: Matrix - Iteration 4

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -1,14 +1,19 @@
 class Matrix
-  attr_reader :string
-  def initialize(string)
-    @string = string
+  attr_reader :rows, :columns
+  def initialize(input)
+    @rows = extract_rows(input)
+    @columns = extract_columns(rows)
   end
 
-  def rows
-    string.each_line.map { |row| row.split.map(&:to_i) }
+  private
+
+  def extract_rows(input)
+    input.each_line.map do |row|
+      row.split.map(&:to_i)
+    end
   end
 
-  def columns
+  def extract_columns(rows)
     rows.transpose
   end
 end
