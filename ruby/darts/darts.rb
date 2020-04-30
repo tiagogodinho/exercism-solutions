16fab596d83d019472c3a3972d3db7ee97e3ea1a
Tiago Godinho <tiagogodinho3@gmail.com>
2020-04-30 16:10:58 -0300

    Ruby: Darts - Iteration 2

--- ruby/darts/darts.rb
+++ ruby/darts/darts.rb
@@ -7,23 +7,17 @@ class Darts
   end
 
   def score
-    scores.find do |range, _|
-      range.cover?(distance_to_the_center)
-    end[1]
+    case radius
+    when 0..1 then 10
+    when 1..5 then 5
+    when 5..10 then 1
+    else 0
+    end
   end
 
   private
 
-  def scores
-    {
-      0.0..1.0 => 10,
-      1.0..5.0 => 5,
-      5.0..10.0 => 1,
-      10.0..1.0/0 => 0,
-    }
-  end
-
-  def distance_to_the_center
+  def radius
     Math.sqrt(x * x + y * y)
   end
 end
