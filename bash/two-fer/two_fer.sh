ad7b13582ba6068d48f35b5ce57a6234e17ca598
Tiago Godinho <tiagogodinho3@gmail.com>
2020-10-07 17:21:20 -0300

    WIP

--- bash/two-fer/two_fer.sh
+++ bash/two-fer/two_fer.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-if [ "$#" -eq 0 ]; then
-  NAME="you"
-else
-  NAME="$1"
-fi
-echo "One for ${NAME}, one for me."
+# DEFAULT="you"
+# NAME=${1:-$DEFAULT}
+
+# echo "One for ${NAME}, one for me."
+
+FOO=${VARIABLE:-default}
+
+echo "${VARIABLE}"
