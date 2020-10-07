dea910369e24dedc8f9d369cd0abf98e4f6dddfe
Tiago Godinho <tiagogodinho3@gmail.com>
2020-10-07 18:21:38 -0300

    Ruby: Nucleotide Count

---
+++ ruby/nucleotide-count/nucleotide_count.rb
@@ -0,0 +1,24 @@
+class Nucleotide
+  def self.from_dna(strand)
+    raise ArgumentError if !strand.empty? && !strand.match?(/^[AGCT]+$/)
+    new(strand)
+  end
+
+  attr_reader :nucleotides
+
+  def initialize(strand)
+    @nucleotides = { 'A' => 0, 'T' => 0, 'C' => 0, 'G' => 0 }
+
+    strand.each_char do |char|
+      nucleotides[char] += 1
+    end
+  end
+
+  def count(nucleotide)
+    nucleotides[nucleotide]
+  end
+
+  def histogram
+    nucleotides
+  end
+end
